4a476731534dd717f3d941cadd13362a629cf3ae
Doncepillo <daniel.perezz@correoaiep.cl>
2019-05-28 21:05:08 -0400

    Cambio notificaciones

--- MasterDetail/Views/Detail.xaml.cs
+++ MasterDetail/Views/Detail.xaml.cs
@@ -17,35 +17,9 @@ namespace MasterDetail
             InitializeComponent();
             Bienvenido.Text = string.Format("Bienvenid@ : {0} {1}", empaque.FirstName, empaque.LastName);
         }
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
 
-            MessagingCenter.Subscribe<object, string>(this, App.NotificationReceivedKey, OnMessageReceived);
-            btnSend.Clicked += OnBtnSendClicked;
-        }
-        void OnMessageReceived(object sender, string msg)
-        {
-            Device.BeginInvokeOnMainThread(() => {
-                lblMsg.Text = msg;
-            });
-        }
 
-        async void OnBtnSendClicked(object sender, EventArgs e)
-        {
-            Debug.WriteLine($"Sending message: " + txtMsg.Text);
-
-            var content = new StringContent("\"" + txtMsg.Text + "\"", Encoding.UTF8, "application/json");
-            //var result = await _client.PostAsync("xamunotifications", content);
-            // Debug.WriteLine("Send result: " + result.IsSuccessStatusCode);
-        }
-
-        protected override void OnDisappearing()
-        {
-            base.OnDisappearing();
-            MessagingCenter.Unsubscribe<object>(this, App.NotificationReceivedKey);
-        }
-
+       
         private async void ListaTurnos(object sender, EventArgs e)
         {
             App.MasterD.IsPresented = false;
--- MasterDetail/Views/User/MisNotificaciones.xaml.cs
+++ MasterDetail/Views/User/MisNotificaciones.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
 using Xamarin.Forms;
 
 namespace MasterDetail
@@ -12,5 +14,35 @@ namespace MasterDetail
             InitializeComponent();
             NavigationPage.SetBackButtonTitle(this, "MiTurnoAPP");
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            MessagingCenter.Subscribe<object, string>(this, App.NotificationReceivedKey, OnMessageReceived);
+            btnSend.Clicked += OnBtnSendClicked;
+        }
+        void OnMessageReceived(object sender, string msg)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                lblMsg.Text = msg;
+            });
+        }
+
+        async void OnBtnSendClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"Sending message: " + txtMsg.Text);
+
+            var content = new StringContent("\"" + txtMsg.Text + "\"", Encoding.UTF8, "application/json");
+            //var result = await _client.PostAsync("xamunotifications", content);
+            // Debug.WriteLine("Send result: " + result.IsSuccessStatusCode);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<object>(this, App.NotificationReceivedKey);
+        }
+
     }
 }
