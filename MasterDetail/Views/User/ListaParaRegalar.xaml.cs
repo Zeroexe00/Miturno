a759c801b86a8722cb286adfde6ce5614202a7e0
Zeroexe00 <exe_6084@hotmail.com>
2019-06-18 15:58:06 -0400

    cambios

--- MasterDetail/Views/User/ListaParaRegalar.xaml.cs
+++ MasterDetail/Views/User/ListaParaRegalar.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using MasterDetail.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using static MasterDetail.ViewModels.ListaEmpaquesRegalarTurnoViewModel;
 
 namespace MasterDetail.Views.User
 {
@@ -18,5 +20,32 @@ namespace MasterDetail.Views.User
             InitializeComponent();
             BindingContext = new ListaEmpaquesRegalarTurnoViewModel(id,spmt,TurnID);
         }
+
+        private void Handle_Toggled(object sender, ToggledEventArgs e)
+        {
+            ObservableCollection<Empaque> emp = new ObservableCollection<Empaque>();
+            var items = ListaEmp.ItemsSource;
+            if (swtAll.IsToggled)
+            {
+                foreach (var item in (ObservableCollection<Empaque>)ListaEmp.ItemsSource)
+                {
+                    item.IsChecked = true;
+                    emp.Add(item);
+
+                }
+                ListaEmp.ItemsSource = emp;
+            }
+            else
+            {
+                foreach (var item in (ObservableCollection<Empaque>)ListaEmp.ItemsSource)
+                {
+                    item.IsChecked = false;
+                    emp.Add(item);
+
+                }
+                ListaEmp.ItemsSource = emp;
+            }
+        }
     }
+
 }
